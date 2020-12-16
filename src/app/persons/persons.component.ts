11c0012582d56af6eefffdaf73a4e5aca72cad54
James Monks <J.monks23@gmail.com>
2020-12-16 11:39:33 +0000

    injected the personsService into the personsComponent constructor which is then allowing for the personsList to be displayed in the browser

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { PersonsService } from "./persons.service";
 
 @Component({
   selector: "app-persons",
@@ -6,5 +7,13 @@ import { Component, Input } from '@angular/core';
 })
 
 export class PersonsComponent {
-  @Input() personList: string[];
+  personList: string[];
+
+
+  // constructor will run automaticlly whenever angular creates a new instance of this component (rendering)
+  // type definition has to be the service, so angular can resolve the service
+  // allowing PersonService to be made available inside the constructor
+  constructor(prsService: PersonsService) {
+    this.personList = prsService.persons;
+  }
 }
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
 // can inject services into other components (dependency injection)
-@Injectable({providedIn: 'root'}) // makes angular (the root) aware of this service
-export class PersonService {
+@Injectable({ providedIn: 'root' }) // makes angular (the root) aware of this service
+export class PersonsService {
   persons: string[] = ["James", "Bob", "Jane"];
 
   addPerson(name: string) {
