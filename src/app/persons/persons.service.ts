21e43aa1502b1f7bb538c4071389804fe54989af
James Monks <J.monks23@gmail.com>
2020-12-16 12:04:11 +0000

    personInputComponent is now using personsService to add a person to the personsList

--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
 
 // can inject services into other components (dependency injection)
 @Injectable({ providedIn: 'root' }) // makes angular (the root) aware of this service
 export class PersonsService {
+  @Output() personCreate = new EventEmitter<string>();
   persons: string[] = ["James", "Bob", "Jane"];
+  enteredPersonName = "";
 
   addPerson(name: string) {
     this.persons.push(name);
+    console.log(this.persons);
   }
 }
